350845307bef95edf423c7d1a45f2acbfc7d0dd2
zhuxiaofeng <zhuxiaofeng@shopex.cn>
2019-08-17 12:05:22 +0800

    travis init

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int main(int argc, char const *argv[])
+{
+	int input;
+	std::cout << "please input a inteager" << endl;
+	std::cin >> input;
+	std::cout << input;
+	return 0;
+}
